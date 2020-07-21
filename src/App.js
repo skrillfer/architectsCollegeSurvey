0cad6815302f77f3e093e171a7b923c877d805fc
Luis Fernando Ramirez Santos <luisramirez@infoutilitygt.com>
2020-07-21 14:43:49 -0600

    finish survey app

--- src/App.js
+++ src/App.js
@@ -2,26 +2,31 @@ import React from 'react';
 import './App.css';
 import FormGeneric from './components/form/form.component';
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import logo from './logo.jpeg';
+
 import { Layout, Menu } from 'antd';
 const { Header, Content, Footer } = Layout;
 
+/*
+<div className="logo" />
+*/
 function App() {
   return (
     <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
-      <div className="logo" />
+    <Header style={{position: 'fixed', zIndex: 1, width: '100%' }}>
+      
+      <img  className="logo" src={logo} alt="My logo" />
       <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">Registro de informacion</Menu.Item>
+        
       </Menu>
     </Header>
-    <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
-      <div className="site-layout-background" style={{ padding: 24 }}>
+    <Content className="site-layout" style={{ marginTop: 64, width:"-webkit-fill-available" }}>
+      <div className="site-layout-background" style={{ padding: 24, width: "100%" }}>
         <FormGeneric/>
       </div>
     </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    <Footer style={{ textAlign: 'center' }}>InfoUtility ©2020</Footer>
   </Layout>
   );
 }
